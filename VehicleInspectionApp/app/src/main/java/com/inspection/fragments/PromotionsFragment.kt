7e84f2d1d18a4f66c6180ee8217c5ba55a101775
sherify88 <sherifyousry88@gmail.com>
2018-05-13 17:25:22 +0200

    adjusting and work to complete scope of services forms

--- VehicleInspectionApp/app/src/main/java/com/inspection/fragments/PromotionsFragment.kt
+++ VehicleInspectionApp/app/src/main/java/com/inspection/fragments/PromotionsFragment.kt
@@ -21,5 +21,29 @@ class PromotionsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_promotions, container, false)
     }
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private val ARG_PARAM1 = "param1"
+        private val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentARRAVFacility.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String, param2: String): PromotionsFragment {
+            val fragment = PromotionsFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
 
 }// Required empty public constructor
--- VehicleInspectionApp/app/src/main/java/com/inspection/fragments/VehiclesFragmentInScopeOfServicesView.kt
+++ VehicleInspectionApp/app/src/main/java/com/inspection/fragments/VehiclesFragmentInScopeOfServicesView.kt
@@ -22,4 +22,28 @@ class VehiclesFragmentInScopeOfServicesView : Fragment() {
         return inflater.inflate(R.layout.fragment_vehicles_fragment_in_scope_of_services_view, container, false)
     }
 
+    companion object {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private val ARG_PARAM1 = "param1"
+        private val ARG_PARAM2 = "param2"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentARRAVFacility.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(param1: String, param2: String): VehiclesFragmentInScopeOfServicesView {
+            val fragment = VehiclesFragmentInScopeOfServicesView()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
 }// Required empty public constructor
